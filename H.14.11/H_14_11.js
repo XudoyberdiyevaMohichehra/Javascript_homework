2f3e518f089bb4b1d4ccbcd5c7946c5c08e929c1
Mohichehra <xudoyberdiyevem25@gmail.com>
2022-11-17 16:37:09 +0300

    If else, Ternary, Nullish

---
+++ H.14.11/H_14_11.js
@@ -0,0 +1,75 @@
+// TASK 1================================================
+
+// let a = 'script';
+
+// if (a == 'EcmaScript') {
+//     console.log('Right');
+
+// }else if(a != 'EcmaScript'){
+//     console.log('you dont know EcmaScript');
+// }
+
+
+
+
+
+
+// TASK 2================================================
+// let a = 60;
+// let b = 50;
+
+// let f = -2;
+
+// if (f % 2 == 0 && f > 0) {
+//     console.log('musbat juft son');
+// }else if(f % 2 != 0 && f > 0){
+//     console.log('musbat toq son');
+// }else if (f % 2 == 0 && f < 0) {
+//     console.log('manfiy juft son');
+// }
+// else if(f % 2 != 0 && f < 0){
+//     console.log('manfiy toq son');
+// }
+
+
+
+// TASK 3 ================================================
+
+// let a = 1;
+// let b = 9;
+// let result = '';
+
+// if (a + b < 4) {
+//     console.log('Below');
+// }else{
+//     console.log('Over');
+// }
+
+
+
+// TASK 3 ================================================
+
+// let login = 'Director';
+
+// if (login == 'Employee') {
+//     console.log('Hello');
+// }else if(login == 'Director'){
+//     console.log('Greetings');
+// }else if(login == ''){
+//     console.log('No login');
+// }else{
+//     console.log('');
+// }
+
+let login = 'Director';
+
+login == 'Employee' 
+? console.log('Hello') 
+: login == 'Director'
+? console.log('Greetings')
+: login == ''
+? console.log('No login')
+:  console.log('');
+
+
+
