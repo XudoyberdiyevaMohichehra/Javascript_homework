cdd4cf401ab5145dc5f0cb27ffc3cd038acdbb73
Mohichehra <xudoyberdiyevem25@gmail.com>
2022-11-17 16:26:53 +0300

    Conversions, Logic operators

---
+++ H.12_11/H_12_11.js
@@ -0,0 +1,58 @@
+// homework
+
+// let a = 1, b = 1;
+
+// let c = ++a;     
+// 1 chiqadi chunki ++ o'zgaruvchidan keyin kelganda 2ta amalni bir vaqtda bajarolmaydi 
+// let d = b++;    
+ // 1 chiqadi chunki ++ o'zgaruvchidan keyin kelganda 2ta amalni bir vaqtda bajarolmaydi
+   
+// console.log(d);
+
+// ==========================================================================================================
+
+
+// 3-ASSIGNMENT RESULT
+
+// let num = 2;
+
+// let x = 1 + (num *= 2)
+// console.log(x);    
+
+//  javob  5 chiqadi chunki  *= ko'payturuv vazifasini bajaradi.
+
+
+
+// ===============================================================================================================
+
+// 4 -type Conversion
+
+// console.log("" + 1 + 0);  => type string(+ borligi uchun) javob 10 chunki  stringa aylangani uchun xuddi matn kabi qushilib qoldi
+// console.log("" - 1 + 0);  => type number (- borligi uchun) javob -1 chunki string bush.
+// console.log(true + false); => type number  javab 1
+// console.log(6 / "3");  => type number chunki bulish amali bulgani uchun avtomatik numberga utkazdi javob 2
+// console.log("2" * "3"); => type number chunki ko'paytirish amali bulgani uchun avtomatik numberga utkazdi javob 6
+// console.log(4 + 5 + "px"); => type string javob 9px chunki chapdan o'nga qarab o'qib kelganligi uchun hisoblashga ulguradi va stringa duch kelib stringa aylanadi
+// console.log("$"+ 4 + 5); => type string javob $45 chunki string birinchi kelganligi uchun amalni bajarolmadi
+// console.log("4" - 2);=> type number chunki minus amali bulgani uchun avtomatik numberga utkazdi javob 2
+// console.log("4px" - 2); type number javob NaN chunki stringga harf aralashgan
+// console.log("-9" + 5); type string javob -95
+// console.log("-9" - 5); type number javob -14
+// console.log(null + 1); type number chunki number avtomatik nullni numberga otkazadi javob 1
+// console.log(undefined + 1); type number javob NaN
+// console.log("\t \n" + 2); type string javob 2 chunki \t \n bular terminalda kurinmaydi
+
+
+// ===============================================================================================================
+
+// 5  Comparisons
+
+// console.log(5 > 4); ==> true
+// console.log("apple" > "pineapple"); ==> false
+// console.log("2" > "p12");   ==> false
+// console.log(undefined == null); ==> true
+// console.log( null == "\n0\n"); ==> false
+// console.log( null === +"\n0\n");  ==> false
+
+console.log(web);
+
