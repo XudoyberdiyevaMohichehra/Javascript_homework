73e3ba00954204a90b5916a6dde5e82ddf188663
Mohichehra <xudoyberdiyevem25@gmail.com>
2022-11-23 20:43:30 +0300

    object1 problems

---
+++ H.22.11/script.js
@@ -0,0 +1,61 @@
+// TASK 1
+// const user = {};
+
+// user.name = "John";
+// user.surname = "Smith";
+// user.name = "Pete";
+// delete user.name
+// console.log(user);
+// ==========================================================================================
+
+// TASK 2
+// let schedule = {};
+
+// console.log( isEmpty(schedule) ); // true
+// schedule["8:30"] = "get up";
+// console.log( isEmpty(schedule) ); // false
+
+// function isEmpty(schedule) {
+    // for (let key in schedule) {
+      // if the loop has started, there is a property
+    //   return false;
+//     }
+//     return true;
+//   }
+// console.log(isEmpty(schedule));
+// ===================================================================================================
+
+// TASK 3
+
+// let salaries = {
+//     John : 100,
+//     Anna : 160,
+//     Pete : 130
+// }
+
+//   let sum = 0;
+// for(let i in salaries){
+//     sum += salaries[i]
+//  }
+//  console.log(sum);
+// ===================================================================================================
+
+// TASK 4
+// let menu = {
+//     width : 200,
+//     height : 300,
+//     title : "My menu"
+// };
+
+// function multiplyNumeric() {
+//     for(let i in menu){
+
+//         if(typeof menu[i] == 'number'){
+//              console.log(menu[i] *= 2);
+//         }
+//     }
+// }
+// multiplyNumeric();
+
+
+
