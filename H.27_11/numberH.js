c09704cea2e94c5cbd3d22cd1a78a90cff2e9ee6
Mohichehra <xudoyberdiyevam25@gmail.com>
2022-12-02 19:58:08 +0300

    binary, min-max

---
+++ H.27_11/numberH.js
@@ -0,0 +1,46 @@
+// task 1 Istalgan sonni binary ko'rishiniga o'tkazib nechta 0 yoki 1 qatnashganini aniqlash.
+// function checkBinary(son) {
+//     let res = son.toString(2)
+//     let num0 = 0;
+//     let num1 = 0;
+//     for(char of res){
+//         if (char === "0") num0++
+//         else num1 ++
+//     }
+//     return `${num1} ta 1 qatnashgan, ${num0} ta 0 qatnashgan`
+// }
+
+// console.log(checkBinary(4));
+// ==========================================================================================
+
+// TASK 2  berilgan sonlar ichida eng katta va eng kichigini topib, ularnni ayirmasini toping.
+
+// let arr = [4, -9, 33, 45 , -67, 22, 100]
+// function findMaxMin(arr) {
+
+//     let min = Math.min(...arr)
+//     let max = Math.max(...arr)
+//     return `${max} - ${min} = ${max - min}`
+// }
+// let arr = [4, -9, 33, 45 , -67, 22, 100]
+// console.log(findMaxMin(arr));
+// ================================================================================================
+// TASK 3  Mathning trunc, parseInt, floor, ceilning farqini tushuntiring.
+
+// Math.parseInt()---> qoldiq qismini butunlay olib tashlab, butun qismini chiqaradi.
+// Math.trunc()---> qoldiq qismini butunlay olib tashlab, butun qismini chiqaradi.
+// Math.floor()---> qoldiq qismida qanchalik yuqori yoki past qiymat bo'lishiga qaramay, pastga qarab yaxlitlaydi. 
+// Math.ceil()---> qoldiq qismida qanchalik ko'p yoki kam qiymat bo'lishiga qaramay, yuqoriga qarab yaxlitlaydi.
+// =================================================================================================
+
+// TASK 4   randomInteger(min,max) function yozilsin. 1 dan 50 gacha bo'lgan ixtiyoriy raqam chiqsin.
+
+// function randomInteger(min, max) {
+//     return Number(Math.ceil(Math.random()* max))
+// }
+// console.log(randomInteger(1, 50));
+
+
+
+
+
